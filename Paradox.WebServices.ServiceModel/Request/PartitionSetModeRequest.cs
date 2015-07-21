b39d8154785443bf3a4ed61749bf639619fedfe1
Keith <keith@bitbounce.ca>
2015-07-21 18:08:14 -0400

    missed file

---
+++ Paradox.WebServices.ServiceModel/Request/PartitionSetModeRequest.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStack;
+
+namespace Paradox.WebServices.ServiceModel.Request
+{
+    [Route("/setpartitionmode", "PUT", Summary = "Set the alarm partition mode (arm, disarm, instant, force, stay)")]
+   
+    public class PartitionSetModeRequest
+    {
+        public int PartitionId { get; set; }
+        public ArmingMode Mode { get; set; }
+    }
+}
